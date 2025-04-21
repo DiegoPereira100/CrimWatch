79ff6c957c6c4d7fddd71de70301e0466cdb179b
diego <diegopgcds@gmail.com>
2025-04-21 20:28:37 -0300

    basics configurations

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import './Home.css'
+
+export default function Home() {
+  return (
+    <section className="home">
+      <h1>Bem-vindo ao Nosso Site</h1>
+      <p>Esta é a página inicial do nosso projeto React.</p>
+    </section>
+  )
+}
